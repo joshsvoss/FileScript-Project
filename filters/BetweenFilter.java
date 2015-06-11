3797f71f3dae17ef4eba27a9d31a60b641b54a32
Josh Voss <joshsvoss@gmail.com>
2015-06-11 23:14:00 +0300

    Impelemented rest of between filter

--- filters/BetweenFilter.java
+++ filters/BetweenFilter.java
@@ -1,14 +1,17 @@
 package filters;
 
+import java.io.File;
+
 public class BetweenFilter implements Filter {
 	
+	private static final int K_BYTES = 1024; 
 	int floor;
 	int roof;
 
 	public BetweenFilter(String floorString, String roofString) throws InvalidFilterParamException {
 		try {
-			this.floor = Integer.parseInt(floorString);
-			this.roof = Integer.parseInt(roofString);
+			this.floor = Integer.parseInt(floorString) * K_BYTES;
+			this.roof = Integer.parseInt(roofString) * K_BYTES;
 		}
 		catch(NumberFormatException e) {
 			throw new InvalidFilterParamException("The param passed to the greater_then filter"
@@ -18,8 +21,16 @@ public class BetweenFilter implements Filter {
 
 	@Override
 	public boolean doesPass(String filepath) {
-		// TODO Auto-generated method stub
-		return false;
+		File file = new File(filepath); //TODO do I have to deal with possibility of filepath being to a directory?
+										//TODO no exception can be thrown her from making a file of a nonexistent file?
+		
+		// If the file does not exist,
+		if (file.length() == 0L ) {
+			return false;
+		}
+		else {
+			return (file.length() >= floor && file.length() <= roof);
+		}
 	}
 
 }
