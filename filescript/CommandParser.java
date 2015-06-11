d2634fa203b59b77fddc3358e142e596c1b3e03a
Josh Voss <joshsvoss@gmail.com>
2015-06-11 16:03:07 +0300

    Relegated validation to method.

--- filescript/CommandParser.java
+++ filescript/CommandParser.java
@@ -9,6 +9,9 @@ public class CommandParser {
 	String cmdFilepath;
 	String srcDirPath;
 	
+	File cmdFile;
+	File srcDir;
+	
 	public CommandParser(String cmdFilepath, String srcDirPath) {
 		// TODO should validation of passed in arguemnts be done here?  Or can parser assume that they're takin?
 		
@@ -16,7 +19,20 @@ public class CommandParser {
 		File cmdFile = new File(cmdFilepath);
 		File srcDir = new File(srcDirPath); // File object can represent a directory too
 		
+		// Make sure the filepaths exist and can be used.
+		validateFilepaths();
+		
+		
+		this.cmdFilepath = cmdFilepath;
+		this.srcDirPath = srcDirPath;
+		
+	}
 
+	/** This helper method checks the filepaths to make sure they exist
+	 * and can be used.  
+	 * 
+	 */
+	private void validateFilepaths() {
 		if (!cmdFile.isFile() || !srcDir.isDirectory()) { //TODO should we be ready to receive a file as the source directory?
 			//TODO what exception to happen here?
 			System.err.println("ERROR: the filepaths provided don't point to a" 
@@ -30,11 +46,6 @@ public class CommandParser {
 			System.err.println("ERROR: Command file can't be read.");
 			System.exit(1);
 		}
-		
-		
-		this.cmdFilepath = cmdFilepath;
-		this.srcDirPath = srcDirPath;
-		
 	}
 	
 	public void parseCommands() { //TODO should this return boolean?
