62c4805d870ca9f9c4997837e1184bd00a4404a2
Josh Voss <joshsvoss@gmail.com>
2015-06-11 22:43:23 +0300

    Got rid of 'breaks' in factory.

--- filters/FilterFactory.java
+++ filters/FilterFactory.java
@@ -6,7 +6,7 @@ public class FilterFactory {
 	private static final int FIRST_INDEX = 0;
 	private static final int PARAM_ONE = 1;
 
-	public static Filter buildFilter(String[] paramList) throws FileScriptException {
+	public static Filter buildFilter(String[] paramList) throws filescript.FileScriptException {
 		
 		switch(paramList[FIRST_INDEX]){
 			case "greater_than":
@@ -16,37 +16,37 @@ public class FilterFactory {
 				}
 					
 				return new GreaterThanFilter(paramList[PARAM_ONE]); //TODO what about the param that needs to be somehow passed to the filter, should it just be passed to the method?
-				break;
+				
 			case "between":
 				return new BetweenFilter();
-				break;  // TODO need to get rid of break statements, since they're unreachable?
+				  // TODO need to get rid of break statements, since they're unreachable?
 			case "smaller_than":
 				return new SmallerThanFilter();
-				break;
+				
 			case "file":
 				return new FileFilter();
-				break;
+				
 			case "contains": 
 				return new ContainsFilter();
-				break;
+				
 			case "prefix":
 				return new PrefixFilter();
-				break;
+				
 			case "suffix":
 				return new SuffixFilter();
-				break;
+				
 			case "writable":
 				return new WritableFilter();
-				break;
+				
 			case "executable":
 				return new ExecutableFilter();
-				break;
+				
 			case "hidden": 
 				return new HiddenFilter();
-				break;
+				
 			case "all":
 				return new AllFilter();
-				break;
+				
 			// Otherwise, we've got a string that doesn't match what we expect
 			default:
 				//TODO what exception is supposed to occur here?
