f56d5bea087e1cc966f0a3d4fddc120370f0b709
Josh Voss <joshsvoss@gmail.com>
2015-06-11 14:44:09 +0300

    Added Orders package and have clearer understanding of how the filters will look and the negatingFilter.

--- filters/Filter.java
+++ filters/Filter.java
@@ -1,7 +1,8 @@
 package filters;
 
 public interface Filter { //TODO should This be an abstract class instead, to demand having a "boolean not" field?  But Tirgul says interface
-
+	
+	public boolean doesPass();
 }
 
 // TODO should rest of real filters go in this file, or doesn't classname need to match filename?
