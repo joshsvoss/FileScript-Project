f8703b392bb62bba5a445ebc20958b5c361e28d1
Josh Voss <joshsvoss@gmail.com>
2015-06-12 14:41:44 +0300

    Implemented TypeOrder

--- orders/TypeOrder.java
+++ orders/TypeOrder.java
@@ -1,5 +1,22 @@
 package orders;
 
+import java.io.File;
+
 public class TypeOrder implements Order {
+	// Magic numbers
+	private static final int OFFSET_ONE = 1;
+	private static final String PERIOD = ".";
+
+	@Override
+	public int compare(File f1, File f2) {
+		return getType(f1).compareTo(getType(f2));
+	}
+	
+	private static String getType(File file) {
+		String name = file.getName();
+		int periodIndex = name.lastIndexOf(PERIOD);
+		String type = name.substring(periodIndex + OFFSET_ONE);
+		return type;
+	}
 
 }
