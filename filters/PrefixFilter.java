835a8a28e7f622ec296c05c29824c9ede2d38ea4
Josh Voss <joshsvoss@gmail.com>
2015-06-12 13:51:13 +0300

    Impl. Writable filter.

--- filters/PrefixFilter.java
+++ filters/PrefixFilter.java
@@ -1,5 +1,26 @@
 package filters;
 
+import java.io.File;
+
 public class PrefixFilter implements Filter {
 
+	private String prefixToMatch;
+
+	public PrefixFilter(String filename) throws InvalidFilterParamException {
+		//TODO should null just be matched to a blank filename, or should it be an exception?
+		if (filename == null) {
+			throw new InvalidFilterParamException("String passed to prefix filter is null");
+		}
+		
+		// Otherwise,
+		this.prefixToMatch = filename;
+	}
+	
+	@Override
+	public boolean doesPass(String filepath) {
+		//TODO is the string passed in a an absolute filepath or just relative?  Probably absolute I imagine
+		File file = new File(filepath);
+		return file.getName().startsWith(this.prefixToMatch); //TODO Does startsWith do what we want?  Or does the targil spec want the prefix specifically whats before the period and filetype
+	}
+	
 }
--- filters/WritableFilter.java
+++ filters/WritableFilter.java
@@ -1,5 +1,31 @@
 package filters;
 
+import java.io.File;
+
 public class WritableFilter implements Filter {
+
+	private boolean yes;
+
+	public WritableFilter(String yesOrNo) throws InvalidFilterParamException {
+		if (yesOrNo == null || ( !yesOrNo.equals("YES") && !yesOrNo.equals("NO") )) {
+			throw new InvalidFilterParamException("Param passed to Writable filter wasn't YES or NO, " 
+			+ "or was null");
+		}
+		
+		// Otherwise, 
+		this.yes = yesOrNo.equals("YES");	
+	}
+
+	@Override
+	public boolean doesPass(String filepath) { //TODO should this method expect a file instead of a filename?
+		File file = new File(filepath);
+		
+		if (this.yes) {
+			return file.canWrite();
+		}
+		else {
+			return !file.canWrite();
+		}
+	}
 
 }
