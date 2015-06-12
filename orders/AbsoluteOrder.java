802a89768d8abe069e2faf9a19d9a5c0fadbe595
Josh Voss <joshsvoss@gmail.com>
2015-06-12 15:02:20 +0300

    Finished implementing orders, now onto factory.:

--- orders/AbsoluteOrder.java
+++ orders/AbsoluteOrder.java
@@ -1,5 +1,16 @@
 package orders;
 
+import java.io.File;
+
+/** This order sorts by absolute name, using getAbsolutePath()
+ * @author Joshua Voss
+ *
+ */
 public class AbsoluteOrder implements Order {
+
+	@Override
+	public int compare(File f1, File f2) {
+		return f1.getAbsolutePath().compareTo(f2.getAbsolutePath());
+	}
 
 }
